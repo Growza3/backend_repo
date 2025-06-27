1d0ccf6f726ccdcf14c684b49363f3dabd16082c
Growza3 <arpitap9005@gmail.com>
2025-06-27 09:17:08 +0530

    Fixed cors issue by removing duplicate cors middlewares

--- server.js
+++ server.js
@@ -133,7 +133,6 @@ app.post("/capture-order", async (req, res) => {
 });
 
 // Middleware
-app.use(cors());
 app.use(express.json({ limit: "10mb" })); // ✅ Increase JSON payload limit
 app.use(express.urlencoded({ limit: "10mb", extended: true })); // ✅ Increase URL-encoded data limit
 app.use(express.json());
